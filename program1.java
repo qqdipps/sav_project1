f0a80281f90eb097d9234e56a54b74dd8ebcc354
qqdipps <sdipp007@gmail.com>
2018-08-04 18:20:26 -0700

    1. deleted unnecessary lines

--- program1.java
+++ program1.java
@@ -13,30 +13,9 @@ public class HelloWorld {
  * @param args
  */
 	public static void main(String[] args) {
-//		System.out.println("Hello World!");	
-//		System.out.println(hi);
-//		System.out.print("testbug");
-		int i;
-		double x = 11.4; 
-		i = (int) x; //cast for single occurence
-		System.out.println(i);
-		//System.out.println("sysout control space = ");
-		x=i; 
-		System.out.println(x);
-		short q = 1;
-		short w = 1;
-		short e;
-	
-		q = (short) (q + 1);
-		q++;
-		System.out.println(i);
-		System.out.println(q);
-		i++;
-		System.out.println(i);
-		if((!(6==5)||(3==++i))){
-			System.out.println("yes");
-			System.out.println(i);
-		}
+		System.out.println("Hello World!");	
+		
+		
 		}
 		
 	}
